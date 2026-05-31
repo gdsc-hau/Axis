a3b88f005af57877ead31b40a3cc264507c1f09d
darknecrocities <parejasarronkian@gmail.com>
2026-05-31 23:17:01 +0800

    fix: remove completely custom cursor

--- apps/id-platform/src/app/layout.tsx
+++ apps/id-platform/src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { VT323 } from "next/font/google";
 import "./globals.css";
 import ChatbotWidget from "../components/Chatbot/ChatbotWidget";
-import CustomCursor from "../components/CustomCursor";
+
 
 const vt323 = VT323({
   weight: '400',
@@ -26,7 +26,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={vt323.className}>
-        <CustomCursor />
         {children}
         <ChatbotWidget />
       </body>
